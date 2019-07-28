6dd351b84e528e614532a63dffc9cc4fc568157d
HunJin Kim <ysg01129@naver.com>
2019-07-28 15:51:24 +0900

    init [초기세팅] 상속 관련 세팅 추가

--- app/src/main/java/cookcook/nexters/com/amoogye/views/main/presenter/MainActivity.kt
+++ app/src/main/java/cookcook/nexters/com/amoogye/views/main/presenter/MainActivity.kt
@@ -1,6 +1,5 @@
 package cookcook.nexters.com.amoogye.views.main.presenter
 
-import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import cookcook.nexters.com.amoogye.R
 import cookcook.nexters.com.amoogye.base.BaseActivity
@@ -8,32 +7,30 @@ import cookcook.nexters.com.amoogye.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity(override val layoutRes: Int, override val isUseDatabinding: Boolean) : BaseActivity() {
+class MainActivity : BaseActivity() {
+
+    override val layoutRes = R.layout.activity_main
+    override val isUseDatabinding = true
+
+    private val mainViewModel: MainViewModel by viewModel()
+    private lateinit var binding: ActivityMainBinding
+
     override fun setupViews() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun subscribeUI() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    val mainViewModel: MainViewModel by viewModel()
-    lateinit var binding: ActivityMainBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.mainVM = mainViewModel
-        mainViewModel.context = this
-        mainViewModel.supportFragmentManager = supportFragmentManager
-
         mainViewModel.onNavigationItemSelected(R.id.navigation_main_calc)
 
         bottom_navigation_main.setOnNavigationItemSelectedListener {
             mainViewModel.onNavigationItemSelected(it.itemId)
         }
+    }
 
+    override fun subscribeUI() {
+    }
+
+    override fun onDataBinding() {
+        super.onDataBinding()
+        binding = DataBindingUtil.setContentView(this, layoutRes)
+        binding.mainVM = mainViewModel
+        mainViewModel.context = this
+        mainViewModel.supportFragmentManager = supportFragmentManager
     }
 }
