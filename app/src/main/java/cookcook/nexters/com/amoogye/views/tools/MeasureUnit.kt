6ebe33cec2fab8f885ed9218846552db8092f434
YChaeeun <se_same5315@naver.com>
2019-08-01 22:27:42 +0900

    feature-#7 [계량도구][UI] 리사이클러뷰 UI 작업
    - 계량도구 리사이클러뷰 구현

---
+++ app/src/main/java/cookcook/nexters/com/amoogye/views/tools/MeasureUnit.kt
@@ -0,0 +1,3 @@
+package cookcook.nexters.com.amoogye.views.tools
+
+class MeasureUnit (var measureUnitBold:String="", var measureUnitNormal:String="")
---
+++ app/src/main/java/cookcook/nexters/com/amoogye/views/tools/ToolsRecyclerAdapter.kt
@@ -0,0 +1,37 @@
+package cookcook.nexters.com.amoogye.views.tools
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import cookcook.nexters.com.amoogye.R
+
+class ToolsRecyclerAdapter (private val context: Context, private val measureUnitList: ArrayList<MeasureUnit>) :
+        RecyclerView.Adapter<ToolsRecyclerAdapter.Holder>() {
+    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val unitBold = itemView.findViewById<TextView>(R.id.measureUnit_bold)
+        private val unitNormal = itemView.findViewById<TextView>(R.id.measureUnit_normal)
+
+        fun bind(measureUnit: MeasureUnit) {
+            unitBold?.text = measureUnit.measureUnitBold
+            unitNormal?.text = measureUnit.measureUnitNormal
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(context).inflate(R.layout.fragment_tools_item, parent, false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(measureUnitList[position])
+
+    }
+
+    override fun getItemCount(): Int {
+        return measureUnitList.size
+    }
+}
