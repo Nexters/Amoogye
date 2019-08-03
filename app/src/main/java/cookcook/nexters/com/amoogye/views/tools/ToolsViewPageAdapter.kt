629f4430ff2e18a60eadabf3fccd68462b80a7b2
YChaeeun <se_same5315@naver.com>
2019-08-03 16:10:54 +0900

    feature-#7 [계량도구][UI] 탭레아웃, 뷰페이지 구현
    - 일반계량, 생활계량 탭레이아웃 구현
    - 뷰페이지에 리사이클러뷰 구현

---
+++ app/src/main/java/cookcook/nexters/com/amoogye/views/tools/ToolsViewPageAdapter.kt
@@ -0,0 +1,25 @@
+package cookcook.nexters.com.amoogye.views.tools
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class ToolsViewPageAdapter(fm:FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getItem(position: Int): Fragment {
+        return when(position) {
+            0 -> { ToolsFragment_Normal() }
+            else -> { ToolsFragment_Life() }
+        }
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position) {
+            0 -> "일반계량"
+            else -> "생활계량"
+        }
+    }
+}
