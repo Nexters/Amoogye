30afc62866968a33724229541c2cb102239ecd5d
KM <sodp2@naver.com>
2019-08-03 16:27:30 +0900

    feature-#6 [계산기] 화면 전환 기능 추가
    - 인분 기준의 fragment 생성
    - fragment 전환 기능을 CalculatorViewModel에 추가
    - 재료, 인원 text에 화면 전환에 대한 onClickListener 붙임

--- app/src/main/java/cookcook/nexters/com/amoogye/views/calc/data/CalculatorRepositoryImpl.kt
+++ app/src/main/java/cookcook/nexters/com/amoogye/views/calc/data/CalculatorRepositoryImpl.kt
@@ -1,8 +1,16 @@
 package cookcook.nexters.com.amoogye.views.calc.data
 
 import android.content.Context
+import android.graphics.Color
 import android.widget.Toast
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import cookcook.nexters.com.amoogye.R
+import cookcook.nexters.com.amoogye.base.BaseFragment
 import cookcook.nexters.com.amoogye.views.calc.domain.CalculatorRepository
+import cookcook.nexters.com.amoogye.views.calc.presenter.IngredientFragment
+import cookcook.nexters.com.amoogye.views.calc.presenter.PortionFragment
+import kotlinx.android.synthetic.main.fragment_calc.*
 
 class CalculatorRepositoryImpl : CalculatorRepository {
 
@@ -10,4 +18,12 @@ class CalculatorRepositoryImpl : CalculatorRepository {
         Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
     }
 
+    override fun changeFragment(id: Int, destination: BaseFragment, fragmentManager: FragmentManager) {
+        val transaction = fragmentManager.beginTransaction()
+
+        transaction.replace(R.id.calculator_container, destination)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+
+        transaction.commit()
+    }
 }
