3d8cf285f31a3fcffb215453990fe49507d67275
HunJin Kim <ysg01129@naver.com>
2019-08-04 19:37:47 +0900

    feature-#15 [안경무][안경무] 안경무 했다
    - 안경무 코인
    - 코인 안경무

--- app/src/main/java/cookcook/nexters/com/amoogye/views/calc/presenter/CalculatorViewModel.kt
+++ app/src/main/java/cookcook/nexters/com/amoogye/views/calc/presenter/CalculatorViewModel.kt
@@ -1,6 +1,7 @@
 package cookcook.nexters.com.amoogye.views.calc.presenter
 
 import android.content.Context
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModel
@@ -9,9 +10,53 @@ import cookcook.nexters.com.amoogye.views.calc.domain.CalculatorRepository
 
 class CalculatorViewModel(private val repo: CalculatorRepository) : ViewModel() {
     lateinit var context: Context
-    var flag = 1
+    var flag: Int = 1
+
+    private var ingredientSelected = true
+    private var portionSelected = false
 
     fun gazuaa(text: String) = repo.showToast(context, text)
 
-    fun convertFragment(view: View, fragmentManager: FragmentManager, click: Int) = repo.changeFragment(view, fragmentManager, flag, click)
+    fun convFragment(flag: Int): Int {
+        when (flag) {
+            1 -> {
+                ingredientSelected = !ingredientSelected
+                if (ingredientSelected) {
+                    val dump = this.flag + flag
+                    if (dump != 1) {
+                        this.flag = dump
+                    } else {
+                        ingredientSelected = !ingredientSelected
+                    }
+                } else {
+                    val dump = this.flag - flag
+                    if (dump != 0) {
+                        this.flag = dump
+                    } else {
+                        ingredientSelected = !ingredientSelected
+                    }
+                }
+            }
+            2 -> {
+                portionSelected = !portionSelected
+                if (portionSelected) {
+                    val dump = this.flag + flag
+                    if (dump != 2) {
+                        this.flag = dump
+                    } else {
+                        portionSelected = !portionSelected
+                    }
+                } else {
+                    val dump = this.flag - flag
+                    if (dump != 0) {
+                        this.flag = dump
+                    } else {
+                        portionSelected = !portionSelected
+                    }
+                }
+            }
+        }
+
+        return this.flag - 1
+    }
 }
