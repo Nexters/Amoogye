33322b1d194070c474b8577f818e9185b7bb3ebd
YChaeeun <se_same5315@naver.com>
2019-08-06 16:27:57 +0900

    feature-#7 [계량도구][UI] 계량도구추가 step1 이름 정하기 구현
    - 레이아웃 구현
    - editText 눌렀을 때 아래 다음 버튼 올라오도록 설정 변경
    - editText 눌렀을 때 위 이미지 사라짐
    - (!!) 그 외 화면 눌렀을 때 키보드 내려가는 기능 미구현

--- app/src/main/java/cookcook/nexters/com/amoogye/views/tools/AddUtilNameFragment.kt
+++ app/src/main/java/cookcook/nexters/com/amoogye/views/tools/AddUtilNameFragment.kt
@@ -1,11 +1,16 @@
 package cookcook.nexters.com.amoogye.views.tools
 
+import android.content.Context
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import cookcook.nexters.com.amoogye.R
+import kotlinx.android.synthetic.main.fragment_addutil_1_name_util.*
 
 
 class AddUtilNameFragment : Fragment() {
@@ -26,5 +31,25 @@ class AddUtilNameFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         return inflater.inflate(R.layout.fragment_addutil_1_name_util, container, false)
+
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        edit_txt_name_util.setOnClickListener {
+            img_add_util_1_name.visibility = View.GONE
+        }
+
+        layout_add_util_1_bottom.setOnClickListener {
+            //addUtilCloseKeyboard()
+        }
+
+    }
+
+    //fun addUtilCloseKeyboard() {
+
+       // val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        //inputMethodManager.hideSoftInputFromWindow(view?.windowToken, 0)
+
+    //}
 }
