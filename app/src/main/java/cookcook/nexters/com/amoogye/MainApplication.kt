a523484ab704aef1dab07bcef8c2f23d2e54f5eb
YChaeeun <se_same5315@naver.com>
2019-08-08 23:53:25 +0900

    feature-#21 [계량도구][Realm] Realm 설정 및 초기화

--- app/src/main/java/cookcook/nexters/com/amoogye/MainApplication.kt
+++ app/src/main/java/cookcook/nexters/com/amoogye/MainApplication.kt
@@ -1,6 +1,7 @@
 package cookcook.nexters.com.amoogye
 
 import android.app.Application
+import io.realm.Realm
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -8,7 +9,7 @@ import org.koin.core.context.startKoin
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-
+        Realm.init(this)
         startKoin {
             androidContext(this@MainApplication)
             androidLogger()
