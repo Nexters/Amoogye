a7731a005eed5cca8e98a4b6d87492de66a702cd
YChaeeun <se_same5315@naver.com>
2019-08-09 01:42:36 +0900

    feature-#21 [계량도구][Realm] 일반계량도구 리사이클러뷰 Realm 연결
    - 초기 데이터 설정을 어떻게 해야 할지 몰라서 일단 임의로 데이터 추가하는 함수 insertData() 작성

--- app/src/main/java/cookcook/nexters/com/amoogye/views/tools/tools_list/ToolsRecyclerAdapter.kt
+++ app/src/main/java/cookcook/nexters/com/amoogye/views/tools/tools_list/ToolsRecyclerAdapter.kt
@@ -7,18 +7,24 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import cookcook.nexters.com.amoogye.R
-import cookcook.nexters.com.amoogye.views.tools.NormalMeasureUnit
+import cookcook.nexters.com.amoogye.views.tools.MeasureUnit
+import io.realm.RealmRecyclerViewAdapter
+import io.realm.RealmResults
 
-class ToolsRecyclerAdapter (private val context: Context, private val measureUnitList: ArrayList<NormalMeasureUnit>) :
-        RecyclerView.Adapter<ToolsRecyclerAdapter.Holder>() {
+class ToolsRecyclerAdapter(
+    private val context: Context,
+    measureUnitList: RealmResults<MeasureUnit>,
+    autoUpdate: Boolean
+) :
+    RealmRecyclerViewAdapter<MeasureUnit, ToolsRecyclerAdapter.Holder>(measureUnitList, autoUpdate) {
     inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         private val unitBold = itemView.findViewById<TextView>(R.id.txt_measureUnit_bold)
         private val unitSoft = itemView.findViewById<TextView>(R.id.txt_measureUnit_soft)
 
-        fun bind(measureUnit: NormalMeasureUnit) {
-            unitBold?.text = measureUnit.measureUnitBold
-            unitSoft?.text = measureUnit.measureUnitSoft
+        fun bind(unit: MeasureUnit) {
+            unitBold?.text = unit.unitNameBold
+            unitSoft?.text = unit.unitNameSoft
         }
     }
 
@@ -28,11 +34,11 @@ class ToolsRecyclerAdapter (private val context: Context, private val measureUni
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
-        holder.bind(measureUnitList[position])
+        holder.bind(data!![position])
 
     }
 
     override fun getItemCount(): Int {
-        return measureUnitList.size
+        return if (data == null) 0 else data!!.size
     }
 }
