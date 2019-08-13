947a807946c72d552b5a57472b01c7313f232532
YChaeeun <se_same5315@naver.com>
2019-08-14 08:58:38 +0900

    feature-#28 [계량도구] viewPager 각 페이지 액션 수정
    - 액티비티 끝내고 다시 실행했을 때 왜 인터페이스들이 안먹히는지 모르겠음..ㅠ

--- app/src/main/java/cookcook/nexters/com/amoogye/views/tools/add_tools/AddUtilActivity.kt
+++ app/src/main/java/cookcook/nexters/com/amoogye/views/tools/add_tools/AddUtilActivity.kt
@@ -8,7 +8,8 @@ import androidx.viewpager.widget.ViewPager
 import cookcook.nexters.com.amoogye.R
 import kotlinx.android.synthetic.main.activity_tools_addutil_main.*
 
-class AddUtilActivity : AppCompatActivity(), OnEditTextClickListener, OnOuterTextClickListener, OnCountEnableTrueListener, OnCountEnableFalseListener {
+class AddUtilActivity : AppCompatActivity(), OnEditTextClickListener, OnOuterTextClickListener,
+    OnCountEnableTrueListener, OnCountEnableFalseListener {
     override fun onClickEditText() {
         layout_main_activity_outer_mid.visibility = View.GONE
     }
@@ -44,15 +45,16 @@ class AddUtilActivity : AppCompatActivity(), OnEditTextClickListener, OnOuterTex
             }
 
             override fun onPageSelected(position: Int) {
-                when (position % 3) {
+                when (position) {
+                    0 -> {
+                        moveNextPage()
+                        btn_add_util_next_page.isEnabled = false
+                        btn_add_util_back.visibility = View.INVISIBLE
+                    }
                     1 -> {
-                        btn_add_util_next_page.text = "다음"
+                        moveNextPage()
                         btn_add_util_next_page.isEnabled = true
                         btn_add_util_back.visibility = View.VISIBLE
-                        btn_add_util_exit.visibility = View.VISIBLE
-                        btn_add_util_next_page.setOnClickListener {
-                            view_pager_add_util.setCurrentItem(getItem(1), true)
-                        }
                     }
                     2 -> {
                         btn_add_util_next_page.setOnClickListener {
@@ -62,18 +64,12 @@ class AddUtilActivity : AppCompatActivity(), OnEditTextClickListener, OnOuterTex
                         btn_add_util_next_page.isEnabled = true
                         btn_add_util_back.visibility = View.VISIBLE
                         btn_add_util_exit.visibility = View.INVISIBLE
-
-                    }
-                    else -> {
-                        btn_add_util_next_page.text = "다음"
-                        btn_add_util_back.visibility = View.INVISIBLE
-                        btn_add_util_exit.visibility = View.VISIBLE
                     }
                 }
             }
         })
 
-        // 버튼 눌렀을 때 이전, 다음 프래그먼트 이동
+        // 버튼 눌렀을 때 이전 프래그먼트 이동
         btn_add_util_back.setOnClickListener {
             view_pager_add_util.setCurrentItem(getItem(-1), true)
         }
@@ -103,6 +99,14 @@ class AddUtilActivity : AppCompatActivity(), OnEditTextClickListener, OnOuterTex
             .setNegativeButton("아니오") { _, _ ->
             }
             .show()
+    }
+
+    private fun moveNextPage() {
+        btn_add_util_next_page.text = "다음"
+        btn_add_util_next_page.setOnClickListener {
+            view_pager_add_util.setCurrentItem(getItem(1), true)
+        }
+        btn_add_util_exit.visibility = View.VISIBLE
     }
 
 }
