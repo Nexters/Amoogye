921db4f9e11f8414eb1e784923e32b15e7764564
KM <sodp2@naver.com>
2019-08-19 14:54:57 +0900

    feature-#66 [계산기] editText마다 하단 버튼 바꿔끼움
    - 안쓰는 flag fragment 삭제
    - unit editText, ingredient editText에 맞춰 하단 버튼 변경

--- app/src/main/java/cookcook/nexters/com/amoogye/views/calc/data/CalculatorRepositoryImpl.kt
+++ app/src/main/java/cookcook/nexters/com/amoogye/views/calc/data/CalculatorRepositoryImpl.kt
@@ -1,18 +1,8 @@
 package cookcook.nexters.com.amoogye.views.calc.data
 
 import android.content.Context
-import android.graphics.Color
-import android.view.View
-import android.widget.RelativeLayout
-import android.widget.TextView
 import android.widget.Toast
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
-import cookcook.nexters.com.amoogye.R
 import cookcook.nexters.com.amoogye.views.calc.domain.CalculatorRepository
-import cookcook.nexters.com.amoogye.views.calc.presenter.IngredientFragment
-import cookcook.nexters.com.amoogye.views.calc.presenter.PortionFragment
-import cookcook.nexters.com.amoogye.views.calc.presenter.TwiceFragment
 
 class CalculatorRepositoryImpl : CalculatorRepository {
     override fun showToast(context: Context, text: String) {
